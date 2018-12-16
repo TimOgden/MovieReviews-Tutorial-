ec355440575e4a70b53b38a4372ddb658d771b54
Tim Ogden <togden1999@gmail.com>
2018-12-15 22:51:08 -0500

    Unable to fix issue currently

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -34,15 +34,17 @@ model.add(keras.layers.Dense(16, activation=tf.nn.relu))
 model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
 model.summary()
 
-model.compile(optimizer=tf.train.AdamOptimizer(), loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
 
 x_val = train_data[:10000]
-x_partial_train = train_data[10000:]
+partial_x_train = train_data[10000:]
 
 y_val = train_labels[:10000]
-y_partial_train = train_data[10000:]
+partial_y_train = train_data[10000:]
 
-history = model.fit(x_partial_train, y_partial_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
+history = model.fit(partial_x_train, partial_y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
 
 test_loss, test_acc = model.evaluate(test_data, test_labels)
 
